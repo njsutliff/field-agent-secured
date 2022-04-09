95b6e1da70aa33d79dc8f319fe6f86942f902f2d
njsutliff <njsutliff@gmail.com>
2022-04-09 14:39:59 -0400

    now using react 18 index .js using createRoot, routing working for basic

--- ui-secured/src/App.js
+++ ui-secured/src/App.js
@@ -1,8 +1,24 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-function App() {
+import Home from './components/Home';
+import Agents from './components/Agents';
+import NotFound from './components/NotFound';
+function App() { /*
+   <Route path = "/agents/add" element = {<Home/>} />
+  <Route path = "/agents/edit/:id" element = {<Home/>} />
+  <Route path = "/agents/delete/:id" element = {<Home/>} />
+  <Route path = "/login" element = {<Agents/>} />
+*/
   return (
-    <div className="App">nik</div>
+<Router>
+<Routes>
+  <Route path = "/" element = {<Home/>} />
+  <Route path = "/agents" element = {<Agents/>} />
+  <Route path = "/*" element = {<NotFound/>} />
+
+</Routes>
+
+</Router>
   );
 }
 
